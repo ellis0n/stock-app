b591fddd93ec74c3a5b10660201e8d9ea832dbdc
glen may <ellisonglenmay@gmail.com>
2022-11-15 11:35:06 -0330

    initial commit

---
+++ 00_analysis/stock_analysis_R.R
@@ -0,0 +1,75 @@
+# BUSINESS SCIENCE ----
+# DS4B 202-R ----
+# STOCK ANALYZER APP - DATA ANALYSIS -----
+# Version 1
+
+# APPLICATION DESCRIPTION ----
+# - The user will select 1 stock from the SP 500 stock index
+# - The functionality is designed to pull the past 180 days of stock data
+# - We will implement 2 moving averages - short (fast) and long (slow)
+# - We will produce a timeseries visualization
+# - We will produce automated commentary based on the moving averages
+
+# REPRODUCIBILITY REQUIREMENTS
+# - The functionality is designed to pull the past 180 days of stock data from today's date
+# - Because of this, your analysis may differ from mine
+# - To reproduce my analysis, replace today() with ymd("2019-08-19")
+
+# LIBRARIES ----
+library(plotly)
+library(tidyquant)
+library(tidyverse)
+library(fs)
+
+
+
+# 1.0 GET STOCK LIST ----
+stock_list_tbl <- tq_index("SP500") %>%
+    select(symbol, company) %>%
+    arrange(symbol) %>%
+    mutate(label = str_c(symbol, company, sep = ", ")) %>%
+    select(label)
+stock_list_tbl
+
+get_stock_list<- function(stock_index = "SP500"){
+    tq_index(stock_index) %>%
+        select(symbol, company) %>%
+        arrange(symbol) %>%
+        mutate(label = str_c(symbol, company, sep = ", ")) %>%
+        select(label)
+}
+
+get_stock_list("SP500")
+
+# 2.0 EXTRACT SYMBOL BASED ON USER INPUT ----
+
+user_input %>% str_split(pattern = ", ") %>% pluck(1, 1)
+
+get_symbol_from_user_input <- function(user_input){
+    user_input %>% str_split(pattern = ", ") %>% pluck(1, 1)
+}
+
+"AAPL, Apple Inc." %>% get_symbol_from_user_input
+
+# 3.0 GET STOCK DATA ----
+from <- today() - days(180) 
+to <- today() # 2019-08-20
+
+"AAPL" %>% 
+    tq_get(get = "stock.prices", from = from, to = to) %>%
+    select(date, adjusted) %>%
+    mutate(mavg_short = rollmean(adjusted, k = 5, na.pad = TRUE, align = "right"))
+
+# 4.0 PLOT STOCK DATA ----
+
+
+
+# 5.0 GENERATE COMMENTARY ----
+
+
+
+# 6.0 TEST WORKFLOW ----
+
+
+
+# 7.0 SAVE SCRIPTS ----
