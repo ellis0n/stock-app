bc00b87fd7a7642870a83ac8f8263e621784f4d5
glen may <ellisonglenmay@gmail.com>
2022-11-17 14:37:45 -0330

    nov 17

--- 00_analysis/stock_analysis_R.R
+++ 00_analysis/stock_analysis_R.R
@@ -122,10 +122,43 @@ if(warning_signal){
     str_glue("In reviewing the stock prices of {user_input}, the {n_long}-day moving average is above the {n_short}-day moving average indicating positive trends.")
 }
 
+generate_commentary <- function(data, user_input){
+    warning_signal <- data %>%
+        tail(1) %>%
+        mutate(mavg_warning_flag = mavg_short < mavg_long) %>%
+        pull(mavg_warning_flag)
+    
+    n_short <- data %>% pull(mavg_short) %>% is.na() %>% sum() + 1
+    n_long <- data %>% pull(mavg_long) %>% is.na() %>% sum() + 1
+    
+    if (warning_signal){
+        str_glue("In reviewing the stock prices of {user_input}, the {n_short}-day moving average is below the {n_long}-day moving average indicating negative trends.")
+    } else {
+        str_glue("In reviewing the stock prices of {user_input}, the {n_long}-day moving average is above the {n_short}-day moving average indicating positive trends.")
+    }
+}
 
+generate_commentary(stock_data_tbl, user_input = user_input)
 
 # 6.0 TEST WORKFLOW ----
 
+get_stock_list("SP500")
 
+"ABT, Abbott Laboratories" %>%
+    get_symbol_from_user_input() %>%
+    get_stock_data(from = "2018-01-01",to = "2019-01-01") %>%
+    plot_stock_data()
+    generate_commentary(user_input = "ABT, Abbott Laboratories")
+    
 
 # 7.0 SAVE SCRIPTS ----
+fs:: dir_create("00_scripts")
+    dump(
+        list= c("get_stock_list", "get_symbol_from_user_input", "get_stock_data", "plot_stock_data", "generate_commentary"),
+        file = "00_scripts/stock_analysis_functions.R",
+        append = FALSE)
+    
+    
+    
+    
+    
---
+++ 01_stock_analyzer_layout.R
@@ -0,0 +1,41 @@
+# BUSINESS SCIENCE ----
+# DS4B 202-R ----
+# STOCK ANALYZER APP - LAYOUT -----
+# Version 1
+
+# APPLICATION DESCRIPTION ----
+# - Create a basic layout in shiny showing the stock dropdown, interactive plot and commentary
+
+
+# LIBRARIES ----
+library(shiny)
+library(shinyWidgets)
+
+library(plotly)
+library(tidyquant)
+library(tidyverse)
+
+source(file = "00_scripts/stock_analysis_functions.R")
+
+# UI ----
+
+ui <- fluidPage(
+    title="Stock Analyzer",
+    
+    #1.0 HEADER ----
+    div(
+        h1("Stock Analyzer", "by Business Science"),
+        p("Glen")
+    )
+    
+)
+
+
+# SERVER ----
+
+server <- function(input, output, session){}
+
+# RUN APP ----
+
+shinyApp(ui = ui, server = server)
+
